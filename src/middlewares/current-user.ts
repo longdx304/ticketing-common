485f9de1212d470b52ec82f4fdfec55f83ba9301
Long Do <longdo@dobtech.vn>
2021-10-02 20:24:37 +0700

    moved errors and middlewares from auth

---
+++ src/middlewares/current-user.ts
@@ -0,0 +1,38 @@
+import jwt from 'jsonwebtoken';
+import { Response, Request, NextFunction } from 'express';
+
+interface UserPayload {
+  id: string;
+  email: string;
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: UserPayload;
+    }
+  }
+}
+
+export const currentUser = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  // check if jwt is set
+  if (!req.session?.jwt) {
+    return next();
+  }
+
+  // check if jwt is valid
+  try {
+    const payload = jwt.verify(
+      req.session.jwt,
+      process.env.JWT_KEY!
+    ) as UserPayload;
+
+    req.currentUser = payload;
+  } catch (err) {}
+
+  next();
+};
